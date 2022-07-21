79784fee6a901a3504f5815cad2e2b596fa9b980
lanefoxwood <lanefoxwood@gmail.com>
2022-07-21 21:46:11 +0000

    js blackjack card counter

---
+++ cc.js
@@ -0,0 +1,59 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+//     switch(card) {
+//       case 2:
+//       case 3:
+//       case 4:
+//       case 5:
+//       case 6:
+//         count += 1;
+//         break;
+//       case 7:
+//       case 8:
+//       case 9:
+//         count = count;
+//         break;
+//       case 10:
+//       case 'J':
+//       case 'Q':
+//       case 'K':
+//       case 'A':
+//         count -= 1;
+//         break;
+//     }
+//   if(count > 0) {
+//       return count + " bet";
+//     } else {
+//       return count + " hold";
+//     }
+//   // Only change code above this line
+// }
+
+// console.log(cc(2), cc(3), cc(7), cc('K'), cc('A'));
+
+
+switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
+  console.log(cc(2), cc(3), cc(7), cc('K'), cc('A'));
